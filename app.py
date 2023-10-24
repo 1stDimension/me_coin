11178dbc6631a5b161a32d4fb0195b37bd4bc2b9
1stDimension <23144389+1stDimension@users.noreply.github.com>
2023-10-24 21:45:27 +0200

    Adding exception for handling multiple Neighbors

--- app.py
+++ app.py
@@ -1,36 +1,24 @@
 from typing import Union
 
 from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 import ipaddress
 import json
+import datetime
+
+from models import Neighbor, Item, InnerItem
 
 import cryptography.hazmat.primitives.serialization as serial
 import cryptography.hazmat.primitives.asymmetric.ec as ec
 import cryptography.hazmat.primitives.hashes as hashes
 import cryptography.exceptions as crypto_exceptions
 
-
-class Neighbor(BaseModel):
-    ip: str
-    pub_key: str
-    address: str
-
+NEIGHBOR_LIMIT = 5
 
 app = FastAPI()
 app.neighbors: dict[str, Neighbor] = {}
-
-
-class InnerItem(BaseModel):
-    public_key: str  # PEM
-    their_address: bytes  # hex of PEM
-
-
-class Item(BaseModel):
-    contents: InnerItem
-    sign: str
-
 
 @app.get("/")
 def read_root(request: Request):
@@ -47,26 +35,47 @@ def read_neighbor() -> dict[str, Neighbor]:
 def keep_alive():
     return {"H": "2O"}
 
+class NeighborLimitReached(Exception):
+    def __init__(self, neighbor: list[Neighbor]):
+        self.neighbor = neighbor
+
+@app.exception_handler(NeighborLimitReached)
+async def unicorn_exception_handler(request: Request, exc: NeighborLimitReached):
+    return JSONResponse(
+        status_code=429,
+        content=exc.neighbor,
+    )
+
 
 @app.post("/attach")
 def attach_neighbour(item: Item, request: Request):
     # Get their identity
     # Verify it's them
-    # Settl
-    print("ITEM")
+    # Add them to neighbors if I can
+    # otherwise send 429 and neighbors
     contents = item.contents
-    pub_key = serial.load_pem_public_key(item.contents.public_key)
-    signature = item.sign
+    pub_key = serial.load_pem_public_key(item.contents.public_key.encode())
+    if isinstance(pub_key,ec.EllipticCurvePublicKey):
+        pass
+        # print("It' public key")
+    signature = bytes.fromhex(item.sign)
+    # print(f"signature = {signature}")
     serial_contents = item.contents.model_dump_json().encode()
+    # print(f"serial_contents = {serial_contents}")
     try:
         pub_key.verify(signature, serial_contents, ec.ECDSA(hashes.SHA256()))
     except crypto_exceptions.InvalidSignature as e:
         raise HTTPException(status_code=403,detail="Invalid Signature")
     client = request.client.host
-    neighbor = Neighbor(client, contents.public_key, contents.their_address)
-    # app.neighbor
-    print(client)
-    print(item)
+    neighbor = Neighbor(ip=client, pub_key=contents.public_key, address=contents.their_address)
+    ct = datetime.datetime.now()
+    ts = int(ct.timestamp() * 1000.0)
+    next_check = ts + 20 * 1000
+    print(f"ts = {ts}")
+    # print(neighbor)
+    # print(item)
+    # app.neighbors: 
+
     return {"Hello": "World"}
 
 
